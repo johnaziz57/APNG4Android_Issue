fd8c474b6080e93287e514b887018951b50de6b0
John <john_aziz57@yahoo.com>
2021-01-03 15:12:32 +0100

    Add butterfly webp

--- app/src/main/java/com/example/webptest/MainActivity.kt
+++ app/src/main/java/com/example/webptest/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val imageView = findViewById<ImageView>(R.id.img)
         GlideApp.with(imageView)
-            .load("https://picnic-nl-dev-images.s3-eu-west-1.amazonaws.com/17ed4fbf0c4a4afed459552b78a466b24183a527c6f2d344abc8cb1db20dc1d9/tile-medium.webp")
+            .load(R.drawable.butterfly_small)
             .into(imageView)
     }
 }
